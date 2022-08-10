823f5ea3b99f47b1aacfb219045688a62538cf35
Irfan Vigma Taufik <davigmacode@gmail.com>
2022-08-10 13:28:47 +0700

    Add css utils

---
+++ libs/widgetarian/src/utils/css.ts
@@ -0,0 +1,13 @@
+import type { CSSAttribute } from 'goober'
+import { css } from 'goober'
+import { prefix } from 'inline-style-prefixer'
+
+export { clsx } from 'clsx'
+
+export { css, prefix }
+
+export type { CSSAttribute }
+
+export const createCss = (attrs: CSSAttribute): string => css(attrs)
+
+export default createCss
