25b0d98f15d79ac13a0f4b58c46e884c8277ac48
Irfan Vigma Taufik <davigmacode@gmail.com>
2022-08-20 10:16:16 +0700

    Use ts instead of tsx

--- libs/widgetarian/rollup.config.js
+++ libs/widgetarian/rollup.config.js
@@ -12,8 +12,9 @@ module.exports = (config) => {
     // },
     input: [
       nxConfig.input,
-      join(__dirname, './src/ui/index.tsx'),
-      join(__dirname, './src/hooks/index.tsx'),
+      join(__dirname, './src/hooks/index.ts'),
+      join(__dirname, './src/ui/index.ts'),
+      join(__dirname, './src/utils/index.ts'),
     ],
     output: {
       ...nxConfig.output,
